1ff7eeb26d16ac2251d900f977c28b7c4e1a23fd
unknown <halimf@HOF-HALIMF1.ds.ad.adp.com>
2013-09-20 13:57:33 -0500

    Switched build to use ripple

--- package.bat
+++ package.bat
@@ -1,1 +1,12 @@
-.nuget\NuGet.exe pack NServiceBus.Serializers.SystemXml\NServiceBus.Serializers.SystemXml.csproj
+SETLOCAL
+SET PROJECT=%~p0
+GOTO END
+echo %PROJECT%
+SET VERSION=1.0.0
+SET R=buildsupport\ripple
+%R% clean
+msbuild src\%PROJECT%.sln /target:clean
+msbuild src\%PROJECT%.sln
+%R% create-packages -v %VERSION%
+:END
+ENDLOCAL
